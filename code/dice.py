3b804e6686655d7602c553b13f5e5ff13eb5dc19
Byung Chun Kim <wizardbc@gmail.com>
2023-02-26 14:30:48 +0900

    feature extraction

--- code/dice.py
+++ code/dice.py
@@ -6,39 +6,19 @@ from torchvision.models.feature_extraction import get_graph_node_names, create_f
 from tqdm.auto import tqdm
 
 class DICE:
-  def __init__(self, model, device='cuda:0'):
+  def __init__(self, model, device='cuda:0', mode='energy'):
     model.eval()
     model.to(device)
     self.model = model
     self.device = device
     self._saved_weight = None
-    self.contrib = None
-    self.mode = 'energe'
+    self.mode = mode
 
-  def avg_features(self, id_loader, prog_bar:bool=True):
-    # _, eval_nodes = get_graph_node_names(self.model)
-    nodes = {'view': 'feature'}
-    model_feat = create_feature_extractor(self.model, nodes)
-
-    features = [[] for _ in id_loader.dataset.classes]
-    pbar = tqdm(id_loader) if prog_bar else id_loader
-    with torch.no_grad():
-      for x, y in pbar:
-        x = x.to(self.device)
-        logit = model_feat(x)['feature']
-        for feature, label in zip(logit, y):
-          features[label.item()].append(feature)
-      features = [torch.stack(lst) for lst in features]
-      return torch.cat(features).mean(dim=0, keepdims=True)
-
-  def set_dice_(self, id_loader, p=90, prog_bar:bool=True):
+  def set_dice_(self, avg_features, p=90):
     if self._saved_weight is None:
       self._saved_weight = self.model.fc.weight.data
     weight = self._saved_weight
-    if self.contrib is None:
-      self.contrib = self.avg_features(id_loader, prog_bar) * weight
-    contrib = self.contrib
-
+    contrib = avg_features * weight
     thresh = np.percentile(contrib.cpu().numpy(), p)
     mask = contrib > thresh
     masked_weight = weight * mask
@@ -46,8 +26,10 @@ class DICE:
 
   def forward(self, x):
     with torch.no_grad():
-      if self.mode == 'energe':
+      if self.mode == 'energy':
         return torch.logsumexp(self.model(x), -1)/1000.0  # to make the return values in [0,1] interval.
       if self.mode == 'msp':
         return F.softmax(self.model(x), -1).max(dim=-1)[0]
       return self.model(x)
+
+  __call__ = forward
